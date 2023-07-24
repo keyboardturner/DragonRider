066d4ad2a07dd2982fd0fd9de7e7e0885e88dd0b
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2023-07-24 02:16:14 -0700

    Adjustments to border, vigor animations
    Added a variety of models overlaid on top of the vigor bubbles. Currently the positions are only set to work with the 6 bubbles.

--- DragonRider.lua
+++ DragonRider.lua
@@ -50,15 +50,15 @@ DR.tick2:SetPoint("TOP", DR.statusbar, "TOPLEFT", (60 / 100) * DR.statusbar:GetW
 
 DR.backdropL = DR.statusbar:CreateTexture(nil, "OVERLAY")
 DR.backdropL:SetAtlas("widgetstatusbar-borderleft") -- UI-Frame-Dragonflight-TitleLeft
-DR.backdropL:SetPoint("LEFT", DR.statusbar, "LEFT", -5, 0)
-DR.backdropL:SetWidth(32)
-DR.backdropL:SetHeight(50)
+DR.backdropL:SetPoint("LEFT", DR.statusbar, "LEFT", -7, 0)
+DR.backdropL:SetWidth(35)
+DR.backdropL:SetHeight(40)
 
 DR.backdropR = DR.statusbar:CreateTexture(nil, "OVERLAY")
 DR.backdropR:SetAtlas("widgetstatusbar-borderright") -- UI-Frame-Dragonflight-TitleRight
-DR.backdropR:SetPoint("RIGHT", DR.statusbar, "RIGHT", 5, 0)
-DR.backdropR:SetWidth(32)
-DR.backdropR:SetHeight(50)
+DR.backdropR:SetPoint("RIGHT", DR.statusbar, "RIGHT", 7, 0)
+DR.backdropR:SetWidth(35)
+DR.backdropR:SetHeight(40)
 
 DR.backdropM = DR.statusbar:CreateTexture(nil, "OVERLAY")
 DR.backdropM:SetAtlas("widgetstatusbar-bordercenter") -- _UI-Frame-Dragonflight-TitleMiddle
@@ -71,7 +71,85 @@ DR.statusbar.bg:SetAllPoints(true)
 DR.statusbar.bg:SetVertexColor(.0, .0, .0, .8)
 
 DR.glide = DR.statusbar:CreateFontString(nil, nil, "GameTooltipText")
-DR.glide:SetPoint("TOPLEFT", DR.statusbar, "TOPLEFT", 0, 0)
+DR.glide:SetPoint("LEFT", DR.statusbar, "LEFT", 10, 0)
+
+DR.modelScene1 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene1:SetPoint("CENTER", DR.statusbar, "CENTER", -105, -36)
+DR.modelScene1:SetWidth(43)
+DR.modelScene1:SetHeight(43)
+DR.modelScene1:SetFrameStrata("HIGH")
+
+DR.model1 = DR.modelScene1:CreateActor()
+DR.model1:SetModelByFileID(1100194)
+DR.model1:SetPosition(5,0,-1.5)
+--DR.model1:SetPitch(.3)
+DR.model1:SetYaw(0)
+DR.modelScene1:Show()
+
+DR.modelScene2 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene2:SetPoint("CENTER", DR.statusbar, "CENTER", -65, -36)
+DR.modelScene2:SetWidth(43)
+DR.modelScene2:SetHeight(43)
+DR.modelScene2:SetFrameStrata("HIGH")
+
+DR.model2 = DR.modelScene2:CreateActor()
+DR.model2:SetModelByFileID(1100194)
+DR.model2:SetPosition(5,0,-1.5)
+--DR.model2:SetPitch(.5)
+DR.model2:SetYaw(1)
+DR.modelScene2:Show()
+
+DR.modelScene3 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene3:SetPoint("CENTER", DR.statusbar, "CENTER", -25, -36)
+DR.modelScene3:SetWidth(43)
+DR.modelScene3:SetHeight(43)
+DR.modelScene3:SetFrameStrata("HIGH")
+
+DR.model3 = DR.modelScene3:CreateActor()
+DR.model3:SetModelByFileID(1100194)
+DR.model3:SetPosition(5,0,-1.5)
+--DR.model3:SetPitch(.3)
+DR.model3:SetYaw(2)
+DR.modelScene3:Show()
+
+DR.modelScene4 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene4:SetPoint("CENTER", DR.statusbar, "CENTER", 25, -36)
+DR.modelScene4:SetWidth(43)
+DR.modelScene4:SetHeight(43)
+DR.modelScene4:SetFrameStrata("HIGH")
+
+DR.model4 = DR.modelScene4:CreateActor()
+DR.model4:SetModelByFileID(1100194)
+DR.model4:SetPosition(5,0,-1.5)
+--DR.model4:SetPitch(.3)
+DR.model4:SetYaw(3)
+DR.modelScene4:Show()
+
+DR.modelScene5 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene5:SetPoint("CENTER", DR.statusbar, "CENTER", 65, -36)
+DR.modelScene5:SetWidth(43)
+DR.modelScene5:SetHeight(43)
+DR.modelScene5:SetFrameStrata("HIGH")
+
+DR.model5 = DR.modelScene5:CreateActor()
+DR.model5:SetModelByFileID(1100194)
+DR.model5:SetPosition(5,0,-1.5)
+--DR.model5:SetPitch(.3)
+DR.model5:SetYaw(4)
+DR.modelScene5:Show()
+
+DR.modelScene6 = CreateFrame("ModelScene", nil, DR.statusbar)
+DR.modelScene6:SetPoint("CENTER", DR.statusbar, "CENTER", 105, -36)
+DR.modelScene6:SetWidth(43)
+DR.modelScene6:SetHeight(43)
+DR.modelScene6:SetFrameStrata("HIGH")
+
+DR.model6 = DR.modelScene6:CreateActor()
+DR.model6:SetModelByFileID(1100194)
+DR.model6:SetPosition(5,0,-1.5)
+--DR.model6:SetPitch(.3)
+DR.model6:SetYaw(5)
+DR.modelScene6:Show()
 
 
 C_Timer.NewTicker(.1, function()
@@ -115,8 +193,56 @@ DR.MountEvents = {
     ["PLAYER_CAN_GLIDE_CHANGED"] = true,
     ["COMPANION_UPDATE"] = true,
     ["PLAYER_LOGIN"] = true,
-
 };
+
+DR.vigorEvent = CreateFrame("Frame")
+DR.vigorEvent:RegisterEvent("UNIT_POWER_UPDATE")
+
+
+function DR.vigorCounter()
+    local vigorCurrent = UnitPower("player", Enum.PowerType.AlternateMount)
+    local vigorMax = UnitPowerMax("player", Enum.PowerType.AlternateMount)
+    if vigorCurrent == 0 then
+        DR.modelScene1:Hide()
+        DR.modelScene2:Hide()
+        DR.modelScene3:Hide()
+        DR.modelScene4:Hide()
+        DR.modelScene5:Hide()
+    end
+
+    if vigorCurrent >=1 then
+        DR.modelScene1:Show()
+    else
+        DR.modelScene1:Hide()
+    end
+    if vigorCurrent >=2 then
+        DR.modelScene2:Show()
+    else
+        DR.modelScene2:Hide()
+    end
+    if vigorCurrent >=3 then
+        DR.modelScene3:Show()
+    else
+        DR.modelScene3:Hide()
+    end
+    if vigorCurrent >=4 then
+        DR.modelScene4:Show()
+    else
+        DR.modelScene4:Hide()
+    end
+    if vigorCurrent >=5 then
+        DR.modelScene5:Show()
+    else
+        DR.modelScene5:Hide()
+    end
+    if vigorCurrent >=6 then
+        DR.modelScene6:Show()
+    else
+        DR.modelScene6:Hide()
+    end
+end
+
+DR.vigorEvent:SetScript("OnEvent", DR.vigorCounter)
 
 DR:RegisterEvent("ADDON_LOADED")
 DR:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
@@ -126,12 +252,11 @@ DR:RegisterEvent("PLAYER_CAN_GLIDE_CHANGED")
 DR:RegisterEvent("COMPANION_UPDATE")
 DR:RegisterEvent("PLAYER_LOGIN")
 --DR:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
---DR:RegisterEvent("UNIT_POWER_UPDATE")
 
 
 function DR.setPositions()
     DR.statusbar:ClearAllPoints();
-    DR.statusbar:SetPoint("BOTTOM", UIWidgetPowerBarContainerFrame, "TOP", 0, 0);
+    DR.statusbar:SetPoint("BOTTOM", UIWidgetPowerBarContainerFrame, "TOP", 0, 5);
     DR.statusbar:Show();
 end
 
